95489eb0a7dc774430c48028cd3838d29ebab4e2
srikanth <srikanth.baireddy@outlook.com>
2025-07-18 16:35:19 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -31,7 +31,6 @@ resource "aws_subnet" "public" {
   count = length (var.public_subnet_cidrs)  
   vpc_id               = aws_vpc.main.id
   cidr_block           = var.public_subnet_cidrs[count.index]
-
   availability_zone = local.az_names[count.index]
   map_public_ip_on_launch = true
 
